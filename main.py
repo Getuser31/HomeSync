8bd3cec990c019298ead076dbc71614f810ee9ef
Charles Fournet <11678991+Getuser31@users.noreply.github.com>
2026-03-26 11:03:36 +0100

    Expand allowed origins in CORS middleware to include `homesync.fr` and `www.homesync.fr`.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://homesync.fr", "https://www.homesync.fr"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
